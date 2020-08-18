807e8f851ab8cf6cc7c62f18e4a21c8353769e8d
CemApaydin <cegit config --global user.email cem.apaydin@ug.bikent.edu.tr>
2020-08-18 21:36:32 +0300

    .

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput,
 } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -34,7 +35,11 @@ function App(){
       <View style ={styles.container}>
           <Text> merhaba </Text>
           <Text> merhabana merhaba kardeş </Text>
+          <View style = {{padding : 40}}>
       
+              <TextInput placeholder = "merhaba de bakıyım" style = {{ borderColor:'blue', borderWidth: 1,}} />
+
+          </View>      
       <View>
         <Separator />
       </View>
@@ -43,6 +48,23 @@ function App(){
         color="#f194ff"
         onPress={() => Alert.alert(' yarra yedin ')}
       />
+      <View>
+        <Separator />
+      </View>
+      <View style ={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Button
+        title="Basma"
+        color="#00ff7f"
+        style= {{width: '200%'}}
+        onPress={() => Alert.alert(' Basma amk')}
+      />
+      <Button
+        title="Basma"
+        color="#cd853f"
+        style= {{width: '200%'}}
+        onPress={() => Alert.alert(' Basma amk ')}
+      />
+      </View>
       </View>
   );
 };
