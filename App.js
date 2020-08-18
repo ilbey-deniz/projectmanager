64172788458991755345fbdf6195cc73f0486c81
CemApaydin <cegit config --global user.email cem.apaydin@ug.bikent.edu.tr>
2020-08-18 21:58:53 +0300

    .

--- App.js
+++ App.js
@@ -1,38 +1,21 @@
 
 import React from 'react';
 //import { NavigationContainer } from '@react-navigation/native';
-
+import Deneme from './components/Deneme';
 import {
-  SafeAreaView,
-  StyleSheet,
   Button,
   Alert, 
-  ScrollView,
   View,
   Text,
-  StatusBar,
   TextInput,
 } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    marginHorizontal: 16,
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-});
-const Separator = () => (
-  <View style={styles.separator} />
-);
+
+
 
 function App(){
   return (
-      <View style ={styles.container}>
+      <View style ={Deneme.container}>
           <Text> merhaba </Text>
           <Text> merhabana merhaba kardeş </Text>
           <View style = {{padding : 40}}>
@@ -40,17 +23,13 @@ function App(){
               <TextInput placeholder = "merhaba de bakıyım" style = {{ borderColor:'blue', borderWidth: 1,}} />
 
           </View>      
-      <View>
-        <Separator />
-      </View>
+     
           <Button
         title="Bas bakayım bi bana"
         color="#f194ff"
         onPress={() => Alert.alert(' yarra yedin ')}
       />
-      <View>
-        <Separator />
-      </View>
+
       <View style ={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
           <Button
         title="Basma"
---
+++ components/Deneme.js
@@ -0,0 +1,16 @@
+import rext from 'react';
+import{ View, Text, StyleSheet} from 'react-native';
+
+const Deneme = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      marginHorizontal: 16,
+    },
+    separator: {
+      marginVertical: 8,
+      borderBottomColor: '#737373',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+  });
+  export default Deneme;
