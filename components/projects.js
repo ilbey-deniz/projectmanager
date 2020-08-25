38087d844c468aa796752b6da8d8c839dfb5d119
ilbey-deniz <mustafa.ilbey@hotmail.com>
2020-08-25 16:55:45 +0300

    projectsList - json file added \n projects.js - FlatList added

--- components/projects.js
+++ components/projects.js
@@ -9,12 +9,23 @@ import {
   } from 'react-native';
 
 
+const DATA = require('./projectsList.json');
+
 function ProjectsScreen() {
     
     return (
       <View>
         <ImageBackground source={require('./View/arkaplan_1.jpg')} style={{width: 415, height: 700}}>
-          <Text>Projets</Text>
+          <FlatList 
+          data= {DATA}
+          renderItem= {({item}) => {
+            return (
+            <Text>{item.name}</Text>
+            );
+          }}  
+          >
+
+          </FlatList>
         </ImageBackground>
       </View>
     );
